362f6e53d4613931875740049a977cdb0873f2d6
Gabriel Rodrigues <gabrielragonhaprofissional@outlook.com>
2025-09-17 20:45:29 -0300

    style: adjust card style

--- src/components/MovieCard.tsx
+++ src/components/MovieCard.tsx
@@ -34,16 +34,16 @@ const MovieCard = ({
           <Text className="text-xs text-white font-bold uppercase">
             {Math.round(vote_average)}
           </Text>
+        </View>
 
-          <View className="flex-row items-center justify-between">
-            <Text className="text-xs text-light-300 font-medium mt-1">
-              {release_date?.split("-")[0]}
-            </Text>
+        <View className="flex-row items-center justify-between">
+          <Text className="text-xs text-light-300 font-medium mt-1">
+            {release_date?.split("-")[0]}
+          </Text>
 
-            {/* <Text className="text-xs font-medium text-light-300 uppercase">
+          {/* <Text className="text-xs font-medium text-light-300 uppercase">
               Filme
             </Text> */}
-          </View>
         </View>
       </TouchableOpacity>
     </Link>
