046f8e6763a69f34b3ecf3fae50e156f14183fa3
Gabriel Rodrigues <gabrielragonhaprofissional@outlook.com>
2025-09-23 00:56:08 -0300

    chore: hide status bar

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,10 +1,17 @@
 import { Stack } from "expo-router";
+import React from "react";
+import { StatusBar } from "react-native";
 import "./globals.css";
 
 export default function RootLayout() {
   return (
-    <Stack>
-      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-    </Stack>
+    <>
+      <StatusBar hidden={true} />
+
+      <Stack>
+        <StatusBar hidden={true} />
+        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+      </Stack>
+    </>
   );
 }
